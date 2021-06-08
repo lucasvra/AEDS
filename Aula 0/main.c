a5afb7c07d81b275a92661b81f9e9c72b6589c74
Lucas Alves <lucas.coau@gmail.com>
2021-06-08 18:14:41 +0000

    aula 0

---
+++ Aula 0/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+// AEDs - Algoritmos e Estruturas de Dados
+// Exercícios
+// 1) Encontre a soma dos múltiplos de 3 ou 5 menores que 1000. (Resposta: 233.168)
+// 2) Encontre a soma dos números pares da sequência de fibonnaci menores ou iguais a 4 milhões. (Resposta: 4.613.732)
+// 3) Escreva um programa que imprime Fizz se o número é múltiplo de 3, Buzz se o número é múltiplo de 5 e 
+//    FizzBuzz caso seja multiplo de 3 e 5.
+
+int main()
+{
+
+}
